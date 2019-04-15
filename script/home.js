9f9c2b961bccfa11b2d774f874c517ae023e3baf
Anele Noroita <36598042+anoroita@users.noreply.github.com>
2019-04-15 15:13:19 +0200

    Add files via upload

---
+++ script/home.js
@@ -0,0 +1,6 @@
+function drawDivider(){
+    var canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    context.fillStyle = "rgb(139,69,19)";
+    context.fillRect(10, 20, 1400, 20);
+}
